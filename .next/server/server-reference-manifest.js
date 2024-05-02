632061c9007b482b4a57228de282047400cd6b0c
Prathamesh Mathapati <prathamesh20@navgurukul.org>
2024-05-02 22:41:14 +0530

    main

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WVwImn5hMjltdEOAwmdlVJZgBiBp5HmBk0//qORzd7Y=\"\n}"
---
+++ src/componest/NavaBar.js
@@ -0,0 +1,19 @@
+"use client"
+
+import { useState } from 'react'
+
+const NavaBar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <div className="bg-white">
+   vf
+
+  
+  </div>
+
+  )
+}
+
+export default NavaBar
+
