066779e45399e3406058cad98fd270dadf8c82ea
Prathamesh Mathapati <prathamesh20@navgurukul.org>
2024-05-12 23:12:53 +0530

    # final code

--- src/componest/ManeSection.js
+++ src/componest/ManeSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Banner from '../assets/banner.png';
 import Image from 'next/image';
 import "./ManeSection.css";
@@ -16,170 +16,205 @@ import { FaWordpress } from "react-icons/fa";
 import { SiTypescript } from "react-icons/si";
 import { SiRedux } from "react-icons/si";
 import { FaBootstrap ,FaHtml5} from "react-icons/fa"
-import navgurukul from "../assets/navgurukul_logo.jpg";
 import { Dark } from './Dark';
-import { companyProject } from './projectDel';
+import { companyProject,experience } from './projectDel';
 import { MdOpenInNew } from "react-icons/md";
 import contactUs from '../assets/contact-us.png'
+import emailjs from '@emailjs/browser';
+import { ColorRing } from 'react-loader-spinner';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ManeSection = () => {
+  const [name,setName]=useState('')
+  const [email,setEmail]=useState('')
+  const [number,setNumber]=useState('')
+  const [message,setMessage]=useState('')
+  const [btnDesable,setBtnDesable]=useState(false)
+  const notifyError = (error) => toast.success('🦄 Wow so easy!', {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    transition: 'Bounce',
+    });
+  const sendEmail = () => {
+    if(!name) return notifyError("Fill the name ")
+    setBtnDesable(true)
+    const serviceid="service_begrnd9"
+    const templateid="template_1pgmqaz"
+    const publicKey="uRtF6o2FlQpWLx2lf"
+    const templateFrom={
+      from_name:name,
+      from_email:email,
+      to_name:"Prathamesh mathapati",
+      message:message
+    }
+    emailjs
+      .send(serviceid, templateid, templateFrom, publicKey
+      )
+      .then(
+        () => {
+          setBtnDesable(false)
+          setName('') 
+          setEmail('')
+          setNumber('')
+          setMessage('')
+        
+        },
+        (error) => {
+          setBtnDesable(false)
+          console.log('FAILED...', error);
+        },
+      );
+  };
+  
   return (
 
     <>
-    <section className='mane-section w-pm-80'>
-      <div className='mane-section-frist '>
-        <h1>Hello I'm <span>Prathamesh</span>  <br /> <span>Web</span> Developer <br /> Best for <span>India</span></h1>
-        <div className='botton-icons d-flex'>
-            <Link href="https://github.com/prathamesh-mathapati" target='_blank' className='botton-icons-dark'><FaGithub /></Link>
-            <Link href="mailto:prathamesh20@navgurukul.org" target='_blank'><MdEmail /></Link>
-            <Link href="https://www.linkedin.com/in/prathamesh-mathapati-aaaa80263/?originalSubdomain=in" target='_blank'> <FaLinkedin/></Link>
-            <Link href="https://www.instagram.com/prathamesh_mathapati/" target='_blank'> <BiLogoInstagramAlt/></Link>
-          </div>
-      </div>
-      <div >
-        <Image src={Banner} alt='none' className='banner-img' />
-      </div>
+      <section className='mane-section w-pm-80' id='home'>
+        <div className='mane-section-frist '>
+          <h1>Hello I'm <span>Prathamesh</span>  <br /> <span>Web</span> Developer <br /> Best for <span>India</span></h1>
+          <div className='botton-icons d-flex'>
+              <Link href="https://github.com/prathamesh-mathapati" target='_blank' className='botton-icons-dark'><FaGithub /></Link>
+              <Link href="mailto:prathamesh20@navgurukul.org" target='_blank'><MdEmail /></Link>
+              <Link href="https://www.linkedin.com/in/prathamesh-mathapati-aaaa80263/?originalSubdomain=in" target='_blank'> <FaLinkedin/></Link>
+              <Link href="https://www.instagram.com/prathamesh_mathapati/" target='_blank'> <BiLogoInstagramAlt/></Link>
+            </div>
+        </div>
+        <div >
+          <Image src={Banner} alt='none' className='banner-img' />
+        </div>
 
-    </section>
+      </section>
 
-      <section className='main-skills-section w-pm-80'>
-          <h3>My <strong> Skills</strong></h3>
-          <div className='main-skills-icons'>
-          <span>
-            <RiNextjsFill/>
-            <b>Next js</b>
-          </span>
-          <span className='main-skills-icons-link'>
-            <FaReact/>
-            <b>React js</b>
-          </span>
-          <span>
-            <SiRedux/>
-            <b>Redux</b>
+        <section className='main-skills-section w-pm-80' id='Skills'>
+            <h3>My <strong> Skills</strong></h3>
+            <div className='main-skills-icons'>
+            <span>
+              <RiNextjsFill/>
+              <b>Next js</b>
             </span>
-          <span>
-            <DiJavascript1/>
-            <b>Javascript</b>
-          </span>
-          <span>
-            <SiTypescript/>
-            <b>Typescript</b>
-          </span>
-          <span>
-            <FaHtml5/>
-            <b>HTML</b>  
-          </span>
-          <span>
-            <FaCss3/>
-            <b>CSS</b>  
-          </span>
-          <span>
-            <FaWebflow/>
-            <b>Webflow</b>  
-          </span>
-          <span>
-            <FaWordpress/>
-            <b>Wordpress</b>  
-          </span>
-          <span>
-            <FaBootstrap/>
-            <b>Bootstrap</b>  
+            <span className='main-skills-icons-link'>
+              <FaReact/>
+              <b>React js</b>
             </span>
+            <span>
+              <SiRedux/>
+              <b>Redux</b>
+              </span>
+            <span>
+              <DiJavascript1/>
+              <b>Javascript</b>
+            </span>
+            <span>
+              <SiTypescript/>
+              <b>Typescript</b>
+            </span>
+            <span>
+              <FaHtml5/>
+              <b>HTML</b>  
+            </span>
+            <span>
+              <FaCss3/>
+              <b>CSS</b>  
+            </span>
+            <span>
+              <FaWebflow/>
+              <b>Webflow</b>  
+            </span>
+            <span>
+              <FaWordpress/>
+              <b>Wordpress</b>  
+            </span>
+            <span>
+              <FaBootstrap/>
+              <b>Bootstrap</b>  
+              </span>
 
+            </div>
+        </section>
+
+        <section className='main-express-section' id='Experience'>
+          <div className='w-pm-80'>
+          <h3>My <strong>Experience</strong></h3>
+          {experience.map((ele,index)=>{
+            return ( 
+          <div className='experience-details' key={index}>
+            <div className='experience-details-heading'>
+              <div className='experience-sub-heading'>
+              <Image src={ele.ImgUrl} width={60} height={60}/>
+              <Link href={ele.projectDetails} target='_blnk'>{ele.titel}</Link> 
+              </div>
+              <p>Date {ele.Date}</p>
+            </div>
+            <p className='company-details'>{ele.descripation}</p>
+          </div>)
+          })}
+         
+
+         
           </div>
-      </section>
+        </section>
 
-      <section className='main-express-section'>
-        <div className='w-pm-80'>
-        <h3>My <strong>Experience</strong></h3>
-        <div className='experience-details'>
-          <div className='experience-details-heading'>
-            <div className='experience-sub-heading'>
-            <Image src={navgurukul} width={60} height={60}/>
-            <Link href="https://www.navgurukul.org/" target='_blnk'>Learn web Development at Navgurukul</Link> 
-            </div>
-            <p>Date </p>
+        <section className='main-projects-section' id='Project'> 
+          <div className='w-pm-80'>
+          <h3>My <strong>Projects</strong></h3>
+
+        
+        {
+          companyProject.map((item,index)=>{
+            return( 
+            <div className='projects-left' key={index}>
+            <div className='w-100'><Image src={item.ImgUrl} alt={item.titel} className='project-img'/></div>
+              <div className='w-100'>
+                <h4>0{index+1}</h4>
+                <p>{item.titel}</p>
+                <span>{item.descripation}</span>
+                <Link href={item.projectDetails.projectLogingPageUrl} target="_blank"><MdOpenInNew className='md-opne-new'/></Link>
+                
+              </div>
+          </div>)
+          })
+        }
+        
           </div>
-          <p className='company-details'>have handled the post of mentor and teacher of 10+ students free of cost I am making some demo projects like an E-commerce website, clone of omani food ect.</p>
-        </div>
+        </section>
 
-        <div className='experience-details'>
-          <div className='experience-details-heading'>
-            <div className='experience-sub-heading'>
-            <Image src={navgurukul} width={60} height={60}/>
-            <Link href="https://www.navgurukul.org/" target='_blnk'>Learn web Development at Navgurukul</Link> 
-            </div>
-            <p>Date </p>
-          </div>
-          <p className='company-details'>have handled the post of mentor and teacher of 10+ students free of cost I am making some demo projects like an E-commerce website, clone of omani food ect.</p>
-        </div>
-        <div className='experience-details'>
-          <div className='experience-details-heading'>
-            <div className='experience-sub-heading'>
-            <Image src={navgurukul} width={60} height={60}/>
-            <Link href="https://www.navgurukul.org/" target='_blnk'>Learn web Development at Navgurukul</Link> 
-            </div>
-            <p>Date </p>
-          </div>
-          <p className='company-details'>have handled the post of mentor and teacher of 10+ students free of cost I am making some demo projects like an E-commerce website, clone of omani food ect.</p>
-        </div>
-        <div className='experience-details'>
-          <div className='experience-details-heading'>
-            <div className='experience-sub-heading'>
-            <Image src={navgurukul} width={60} height={60}/>
-            <Link href="https://www.navgurukul.org/" target='_blnk'>Learn web Development at Navgurukul</Link> 
-            </div>
-            <p>Date </p>
-          </div>
-          <p className='company-details'>have handled the post of mentor and teacher of 10+ students free of cost I am making some demo projects like an E-commerce website, clone of omani food ect.</p>
-        </div>
-        </div>
-      </section>
-
-      <section className='main-projects-section'>
-        <div className='w-pm-80'>
-        <h3>My <strong>Projects</strong></h3>
-
-      
-       {
-        companyProject.map((item,index)=>{
-          return( 
-          <div className='projects-left'>
-          <div className='w-100'><Image src={item.ImgUrl} alt={item.titel} className='project-img'/></div>
-            <div className='w-100'>
-              <h4>0{index+1}</h4>
-              <p>{item.titel}</p>
-              <span>{item.descripation}</span>
-               <Link href={item.projectDetails.projectLogingPageUrl} target="_blank"><MdOpenInNew className='md-opne-new'/></Link>
-              
-            </div>
-        </div>)
-        })
-       }
-      
-        </div>
-      </section>
-
-      <section className='contact-us-section'>
-        <div className='w-pm-80'>
-          <h4>Let’s discuss about an project!</h4>
-          <div className='contact-us-form-main'>
-            <div className='from-contect-us'>
-              <p>Connect use</p>
-              <input placeholder='Your name ' type='text'/>
-              <input placeholder='Your Email' type='email'/>
-              <input placeholder='Your Number ' type='number'/>
-              <input placeholder='Your Website ' type='Website'/>
-              <button className='sub-btn'>Submit</button>
-            </div>
-            <div className='map-contect-us'>
-              <Image src={contactUs} alt="contact us" height={484} />
+        <section className='contact-us-section' id='Contact me'>
+          <div className='w-pm-80'>
+            <h4>Let’s discuss about an project!</h4>
+            <div className='contact-us-form-main'>
+              <div className='from-contect-us'>
+                <p>Connect use</p>
+                <input placeholder='Your name ' type='text' onChange={e=>(setName(e.target.value))} value={name}/>
+                <input placeholder='Your Email' type='email' onChange={e=>setEmail(e.target.value)} value={email}/>
+                <input placeholder='Your Number ' type='number' onChange={e=>setNumber(e.target.value)} value={number}/>
+                <textarea placeholder='Your messges ' onChange={e=>setMessage(e.target.value)} value={message}/>
+                <button className='sub-btn' onClick={()=>sendEmail()} disabled={btnDesable}>
+                <ColorRing
+                  visible={btnDesable}
+                  height="30"
+                  width="30"
+                  ariaLabel="color-ring-loading"
+                  wrapperStyle={{}}
+                  wrapperClass="color-ring-wrapper"
+                  colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+                  />Submit</button>
+              </div>
+              <div className='map-contect-us'>
+                <Image src={contactUs} alt="contact us" height={484} />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <Dark/>    </>
+        <Dark/>    
+      </>
   );
 }
 
--- src/componest/NavaBar.js
+++ src/componest/NavaBar.js
@@ -3,10 +3,11 @@ import './style.css'
 import { useState } from "react";
 import { MdOutlineMenu } from "react-icons/md";
 import { RxCross1 } from "react-icons/rx";
+import Link from "next/link";
 
 
 export default function NavaBar() {
-  const anvSubText = ["About us", "Skills", "Project", "Contact me"]
+  const anvSubText = ["Skills", "Experience","Project", "Contact me"]
   const [navSubtext, setnavSubtext] = useState('')
   const [showManu, setsShowManu] = useState(true)
 
@@ -14,12 +15,13 @@ export default function NavaBar() {
   return (
     <nav className={`${showManu && "nav-bar-tag"} nav`}>
       <div className="flex nav-bar-main relative">
-        <div>logo</div>
+        <Link href="#home">logo</Link>
+        
 
         {showManu && <div className="flex nav-sub-text ">
 
           {anvSubText.map(ele => {
-            return (<p className={ele === navSubtext ? "nav-link" : ""} onClick={() => { setnavSubtext(ele) }}>{ele}</p>)
+            return (<Link className={ele === navSubtext ? "nav-link" : ""} onClick={() => { setnavSubtext(ele) }} href={`#${ele}`}>{ele}</Link>)
           })}
           <div className="flex nav-bar-btn d-none-pm">
             <a className="flex justify-between" href="https://www.canva.com/design/DAF-bGm5EIk/gBR5OP9_uTvxgVCV0VhqCQ/view?utm_content=DAF-bGm5EIk&utm_campaign=designshare&utm_medium=link&utm_source=editor" download target="_blank" rel="noopener noreferrer">Resume <IoCloudDownloadOutline /></a>
