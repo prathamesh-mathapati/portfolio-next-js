4dc2ead1d89f55f67aaae7591f79108d0d16b41c
Prathamesh Mathapati <93044026+prathamesh-mathapati@users.noreply.github.com>
2024-05-16 09:34:55 +0530

    add side map

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import ManeSection from "@/componest/ManeSection";
 import NavaBar from "@/componest/NavaBar";
 
 export default function Home() {
+  console.log(window.location.href);
   return (
 
     <main className="main-page">
